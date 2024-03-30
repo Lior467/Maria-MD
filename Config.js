24820e0cd235519cee4b42e72450d2b2bfdda6dd
Lior467 <165521792+Lior467@users.noreply.github.com>
2024-03-30 23:27:12 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,24 +2,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['+43 660 1057068']
+global.ownername = "LIOR"//owner name
+global.ytname = 
+global.socialm = 
+global.location = 
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 4evermidN8 BOT //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '💙𓆩‎✘LIOR✘𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'LIOR'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = 
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://https://chat.whatsapp.com/GBcmGTSbWn3HFTGlkkMoRL
 
 //custom prefix
 global.prefa = ['.']
